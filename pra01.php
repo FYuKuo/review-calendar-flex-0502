3a75debece572d756d4b1cfe602d804e0a7bcaba
FY Kuo <teresa99339@gmail.com>
2022-05-02 19:39:07 +0800

    利用for迴圈將月份第一天和最後一天 前後之空白,放入陣列之中

--- pra01.php
+++ pra01.php
@@ -39,7 +39,7 @@
 <body>
     <h2>0502-複習PHP月曆</h2>
     <?php
-    $month=5; //月份
+    $month=6; //月份
     $firstDay=date("Y-") . $month . "-1"; //月份第一天
     $firstDaySecond=strtotime($firstDay); //月份第一天轉秒
     $firstDayWeek=date('w',$firstDaySecond); //月份第一天星期幾
@@ -61,10 +61,20 @@
 
     $allDate=[]; //要放入所有日期的空陣列
 
+    //利用for迴圈將月份第一天之前的空白放入陣列
+    for($i=0; $i<$firstDayWeek; $i++){
+        $allDate[]="";
+    }
+    
     //利用for迴圈將日期從第一天開始每次加一天,加到月份的最後一天,並放入陣列之中
     for($i=0; $i<$monthDay ;$i++){
         $date=date('Y-m-d',strtotime("+$i days",$firstDaySecond));
         $allDate[]=$date;
+    }
+    
+    //利用for迴圈將月份最後一天之後的空白放入陣列
+    for($i=0; $i<6-$lastDayWeek; $i++){
+        $allDate[]="";
     }
 
     echo "<pre>";
