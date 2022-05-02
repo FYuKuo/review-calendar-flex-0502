409fb600f3676e0e752afd15cfdc85ff643d0a8f
FY Kuo <teresa99339@gmail.com>
2022-05-02 20:31:04 +0800

    加入月份欄位 微調css樣式

--- pra01.php
+++ pra01.php
@@ -12,7 +12,7 @@
 
         .table {
             width: 700px;
-            height: 560px;
+            /* height: 610px; */
             display: flex;
             flex-wrap: wrap;
             align-content: flex-start;
@@ -21,7 +21,7 @@
             margin-top: 10px;
             background-color: #F0F9E9;
             border-radius: 20px;
-            padding-top:10px ;
+            padding-top:5px ;
             box-shadow:1px 1px 25px lightgrey;
         }
         .table>div {
@@ -32,6 +32,15 @@
             margin-top: -1px;
             line-height: 100px;
             text-align: center;
+        }
+        .table>.headerMonth {
+            width: 100%;
+            height: 50px;
+            text-align: center;
+            line-height: 50px;
+            font-size: 20px;
+            font-weight: bold;
+            letter-spacing: 2px;
         }
         .table>.header {
             height: 50px;
@@ -45,6 +54,7 @@
             text-decoration: underline;
             text-underline-offset: 5px;
             text-decoration-color: red;
+            font-weight: bold;
         }
         .weekendH{
            color: red; 
@@ -55,9 +65,9 @@
     <h2>0502-複習PHP月曆</h2>
     <?php
     $month=5; //月份
-    // $monthFont=date('F',strtotime($month));
     $firstDay=date("Y-") . $month . "-1"; //月份第一天
     $firstDaySecond=strtotime($firstDay); //月份第一天轉秒
+    $monthFont=date("F",$firstDaySecond); //月份英文
     $firstDayWeek=date('w',$firstDaySecond); //月份第一天星期幾
     $monthDay=date('t',$firstDaySecond); //月份有幾天
     $lastDay=date('Y-') . $month . "-" . $monthDay; //月份最後一天
@@ -65,7 +75,7 @@
     $lastDayWeek=date('w',$lastDaySecond); //月份最後一天星期幾
 
     echo "月份 ==>" . $month . "<br>";
-    // echo "月份 ==>" . $monthFont . "<br>";
+    echo "月份英文 ==>" . $monthFont . "<br>";
     echo "月份第一天 ==>" . $firstDay . "<br>";
     echo "月份第一天轉秒 ==>" . $firstDaySecond . "<br>";
     echo "月份第一天星期幾 ==>" . $firstDayWeek . "<br>";
@@ -103,7 +113,7 @@
     //包住所有日期之div的外框
     echo "<div class='table'>";
 
-    // echo "<div class='headerMonth'>$month</div>";
+    echo "<div class='headerMonth'>$monthFont</div>";
 
     echo "<div class='header weekendH'>Sun</div>";
     echo "<div class='header'>Mon</div>";
@@ -127,7 +137,7 @@
         //檢查變數$day中是否含有空白 如果沒有就將$day變數轉成日期只有d的格式 並印出來, 沒有的話就只印出div
         if(!empty($day)){
             $dayFont=date("d",strtotime($day));
-            echo "<div class='$checktoday'> {$dayFont} </div>";
+            echo "<div class='$checktoday'>{$dayFont}</div>";
         }else{
             echo "<div></div>";
         }
