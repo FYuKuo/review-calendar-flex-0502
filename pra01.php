f68df764a518157351388c3ad6f886c58efbc100
FY Kuo <teresa99339@gmail.com>
2022-05-02 20:21:35 +0800

    修改月曆css樣式 加入背景顏色 調整邊緣

--- pra01.php
+++ pra01.php
@@ -12,15 +12,20 @@
 
         .table {
             width: 700px;
-            height: 700px;
+            height: 560px;
             display: flex;
             flex-wrap: wrap;
             align-content: flex-start;
+            justify-content: center;
             margin: 0 auto;
             margin-top: 10px;
+            background-color: #F0F9E9;
+            border-radius: 20px;
+            padding-top:10px ;
+            box-shadow:1px 1px 25px lightgrey;
         }
         .table>div {
-            border: 1px solid lightpink;
+            /* border: 1px solid lightgray; */
             width: 100px;
             height: 100px;
             margin-left: -1px;
@@ -33,6 +38,7 @@
             line-height: 50px;
             font-weight: bold;
             text-transform: uppercase; /*小寫轉大寫*/
+            border-bottom: 1px solid black;
         }
         .table>.today {
             color: red;
@@ -40,12 +46,16 @@
             text-underline-offset: 5px;
             text-decoration-color: red;
         }
+        .weekendH{
+           color: red; 
+        }
         </style>
 </head>
 <body>
     <h2>0502-複習PHP月曆</h2>
     <?php
     $month=5; //月份
+    // $monthFont=date('F',strtotime($month));
     $firstDay=date("Y-") . $month . "-1"; //月份第一天
     $firstDaySecond=strtotime($firstDay); //月份第一天轉秒
     $firstDayWeek=date('w',$firstDaySecond); //月份第一天星期幾
@@ -55,6 +65,7 @@
     $lastDayWeek=date('w',$lastDaySecond); //月份最後一天星期幾
 
     echo "月份 ==>" . $month . "<br>";
+    // echo "月份 ==>" . $monthFont . "<br>";
     echo "月份第一天 ==>" . $firstDay . "<br>";
     echo "月份第一天轉秒 ==>" . $firstDaySecond . "<br>";
     echo "月份第一天星期幾 ==>" . $firstDayWeek . "<br>";
@@ -92,22 +103,27 @@
     //包住所有日期之div的外框
     echo "<div class='table'>";
 
-    echo "<div class='header'>Sun</div>";
+    // echo "<div class='headerMonth'>$month</div>";
+
+    echo "<div class='header weekendH'>Sun</div>";
     echo "<div class='header'>Mon</div>";
     echo "<div class='header'>Tues</div>";
     echo "<div class='header'>Wed</div>";
     echo "<div class='header'>Thur</div>";
     echo "<div class='header'>Fri</div>";
-    echo "<div class='header'>Sat</div>";
+    echo "<div class='header weekendH'>Sat</div>";
 
     
     //利用陣列的迴圈將日期印出
     foreach($allDate as $day){
         $checktoday=""; //檢查是否為今天的空變數
         $today=date('Y-m-d'); //今天的日期
+
+        //如果day變數內的日期是今天,就把checktoday變數放入today的字串 並在印出div時 加上class=checktoday變數
         if($day == $today){
             $checktoday='today';
         }
+
         //檢查變數$day中是否含有空白 如果沒有就將$day變數轉成日期只有d的格式 並印出來, 沒有的話就只印出div
         if(!empty($day)){
             $dayFont=date("d",strtotime($day));
@@ -119,5 +135,6 @@
 
     echo "</div>";
     ?>
+    <br><br><br>
 </body>
 </html>
